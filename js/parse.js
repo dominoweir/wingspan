c17807f20f46429eea3ef9ad9f37550593c69517
Domino Rose Weir <dominoweir@gmail.com>
2017-03-22 10:50:09 -0500

    scripting up and running

--- js/parse.js
+++ js/parse.js
@@ -1,18 +1,11 @@
 function parseSubmission(){
-    var formData = document.getElementById("form");
-    var text = "";
-    var i;
-    for (i = 0; i < x.length ;i++) {
-        text += x.elements[i].value + "<br>";
-    }
-    runPyScript(text);
+    var formString = $('#form').serialize();
+    runPyScript(formString);
 }
 function runPyScript(input){
   $.ajax({
     type: "POST",
     url: "../python/getTime.py",
-    data: { param: input},
-    }).done(function( o ) {
-    alert("OK");
-  });
+    data: { param: input}
+    })
 }
