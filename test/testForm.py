f7874cf383a21cc454e22b20c5f68e4a69986a10
Domino Rose Weir <dominoweir@gmail.com>
2017-04-24 19:40:35 -0500

    testForm sort of worksgit status

--- test/testForm.py
+++ test/testForm.py
@@ -1,6 +1,7 @@
 #ScriptName : testForm.py
 #---------------------
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import os
 
 #Following are optional required
@@ -8,7 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 
-baseurl = "../compute.html"
+baseurl = "https://wingspan-app.herokuapp.com/compute.html"
 
 flightNumber = "7"
 startLocation = "University of Texas at Austin, Austin, TX"
@@ -22,7 +23,6 @@ xpaths = { 'flightNumber' : "//input[@name='q1']",
          }
 
 os.environ['PATH'] = "/Users/dominoweir/Documents/wingspan/test"
-print os.environ['PATH']
 driver = webdriver.Chrome()
 driver.get(baseurl)
 driver.maximize_window()
@@ -30,8 +30,11 @@ results = open("formExpected.txt",'w')
 
 # write sample data in to text boxes
 driver.find_element_by_xpath(xpaths['flightNumber']).send_keys(flightNumber)
+driver.find_element_by_xpath(xpaths['next']).send_keys(Keys.RETURN)
 driver.find_element_by_xpath(xpaths['startLocation']).send_keys(startLocation)
+driver.find_element_by_xpath(xpaths['next']).send_keys(Keys.RETURN)
 driver.find_element_by_xpath(xpaths['timingPref']).send_keys(timingPref)
+driver.find_element_by_xpath(xpaths['next']).send_keys(Keys.RETURN)
 driver.find_element_by_xpath(xpaths['withKids']).send_keys(withKids)
 
 # click submit button
