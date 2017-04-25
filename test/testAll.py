bdd90d2365a3d0b7dd7cb27134a375d575a8fb0e
Domino Rose Weir <dominoweir@gmail.com>
2017-04-24 20:13:50 -0500

    wrote part of testing suite

--- test/testAll.py
+++ test/testAll.py
@@ -1,43 +1,27 @@
 import os
+import testForm
 
-# run sign in with testSignIn.py
-os.system("testSignIn.py")
-expected = open("signInExpected.txt", 'r')
-actual = open("signInActual.txt",'r')
+# clear existing output files
+try:
+    os.remove("formActual.txt")
+except OSError:
+    pass
 
-expectedText = expected.read()
-actualText = actual.read()
-
-if (expectedText != actualText)
-    print("Sign in test failed")
-
-expected.close()
-actual.close()
+try:
+    os.remove("mapsActual.txt")
+except OSError:
+    pass
 
 # run form submission with testForm.py
-os.system("testForm.py")
+testForm.run()
 expected = open("formExpected.txt", 'r')
 actual = open("formActual.txt",'r')
 
 expectedText = expected.read()
 actualText = actual.read()
 
-if (expectedText != actualText)
+if (expectedText != actualText):
     print("Form submission test failed")
-
-expected.close()
-actual.close()
-
-# run maps tests with testMaps.py
-os.system("testMaps.py")
-expected = open("mapsExpected.txt", 'r')
-actual = open("mapsActual.txt",'r')
-
-expectedText = expected.read()
-actualText = actual.read()
-
-if (expectedText != actualText)
-    print("Map verification test failed")
 
 expected.close()
 actual.close()
--- test/testForm.py
+++ test/testForm.py
@@ -9,41 +9,44 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 
-baseurl = "https://wingspan-app.herokuapp.com/compute.html"
+def run():
+    baseurl = "https://wingspan-app.herokuapp.com/compute.html"
 
-flightNumber = "7"
-startLocation = "University of Texas at Austin, Austin, TX"
-timingPref = "early"
-withKids = "no"
+    flightNumber = "7"
+    startLocation = "University of Texas at Austin, Austin, TX"
+    timingPref = "early"
+    withKids = "no"
 
-xpaths = { 'flightNumber' : "//input[@name='q1']",
+    xpaths = { 'flightNumber' : "//input[@name='q1']",
            'startLocation' : "//input[@name='q2']",
            'timingPref' :   "//input[@name='q3']",
-           'withKids' : "//input[@name='q4']"
+           'withKids' : "//input[@name='q4']",
+           'next' : "//button[@name='next']",
+           'submitButton' : "//button[@name='submitButton']"
          }
 
-os.environ['PATH'] = "/Users/dominoweir/Documents/wingspan/test"
-driver = webdriver.Chrome()
-driver.get(baseurl)
-driver.maximize_window()
-results = open("formExpected.txt",'w')
+    os.environ['PATH'] = "/Users/dominoweir/Documents/wingspan/test"
+    driver = webdriver.Chrome()
+    driver.get(baseurl)
+    driver.maximize_window()
+    results = open("formActual.txt",'w')
 
-# write sample data in to text boxes
-driver.find_element_by_xpath(xpaths['flightNumber']).send_keys(flightNumber)
-driver.find_element_by_xpath(xpaths['next']).send_keys(Keys.RETURN)
-driver.find_element_by_xpath(xpaths['startLocation']).send_keys(startLocation)
-driver.find_element_by_xpath(xpaths['next']).send_keys(Keys.RETURN)
-driver.find_element_by_xpath(xpaths['timingPref']).send_keys(timingPref)
-driver.find_element_by_xpath(xpaths['next']).send_keys(Keys.RETURN)
-driver.find_element_by_xpath(xpaths['withKids']).send_keys(withKids)
+    # write sample data in to text boxes
+    driver.find_element_by_xpath(xpaths['flightNumber']).send_keys(flightNumber)
+    driver.find_element_by_xpath(xpaths['next']).click()
+    driver.find_element_by_xpath(xpaths['startLocation']).send_keys(startLocation)
+    driver.find_element_by_xpath(xpaths['next']).click()
+    driver.find_element_by_xpath(xpaths['timingPref']).send_keys(timingPref)
+    driver.find_element_by_xpath(xpaths['next']).click()
+    driver.find_element_by_xpath(xpaths['withKids']).send_keys(withKids)
 
-# click submit button
-try:
-    driver.find_element_by_xpath(xpaths['submitButton']).click()
-    results.write("Simple test was a success.")
-except ElementNotVisibleException, e:
-    results.write("Simple test failed.")
-    results.write("Error encountered: " + e)
+    # click submit button
+    try:
+        driver.find_element_by_xpath(xpaths['next']).click()
+        results.write("Simple test was a success.\n")
+    except Exception, e:
+        results.write("Simple test failed.\n")
+        results.write(str(e))
 
-results.close()
-driver.quit()
+    results.close()
+    driver.quit()
